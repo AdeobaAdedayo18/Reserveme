36e8970c61d3ed0c5133a38a91c0cb21d35cf891
AdeobaAdedayo18 <adeobaadedayo18@gmail.com>
2025-02-03 09:23:26 +0100

    Analytics Added

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,12 @@ import { RouterProvider } from "react-router-dom";
 import { Toaster } from "./components/ui/toaster.tsx";
 import "./index.css";
 import { router } from "./routes.tsx";
+import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router}></RouterProvider>
+    <RouterProvider router={router} />
     <Toaster />
+    <Analytics />
   </StrictMode>
 );

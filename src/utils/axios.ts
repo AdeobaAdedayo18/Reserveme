b877b2a9252b12b14c229351304cf3d6437ff7b5
AdeobaAdedayo18 <adeobaadedayo18@gmail.com>
2025-02-10 00:47:45 +0100

    updated logical error

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { baseURL } from "./axiosInstance";
+const baseURL= "https://reserveme.up.railway.app"
 
 export const axiosInstance = axios.create({
     baseURL: baseURL,

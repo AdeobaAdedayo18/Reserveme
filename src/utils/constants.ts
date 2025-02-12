79a790243cd8e8d4beb66d06d8f2d2066ec803f6
AdeobaAdedayo18 <adeobaadedayo18@gmail.com>
2025-02-12 07:31:09 +0100

    Tried to fix some errors

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,8 @@
-const isProduction = process.env.NODE_ENV === 'production';
+// const isProduction = process.env.NODE_ENV === 'production';
 
-export const baseURL = isProduction 
-    ? "https://reserveme.up.railway.app" 
-    : "http://127.0.0.1:8000";
+
+// export const baseURL = isProduction 
+//     ? "https://reserveme.up.railway.app" 
+//     : "http://127.0.0.1:8000";
+
+export const baseURL = "https://reserveme.up.railway.app";
